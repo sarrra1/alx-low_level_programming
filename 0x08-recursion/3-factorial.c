2549873b878fb290a43478d32cf15cd752337449
sarrra1 <sarafaisal0161999@gmail.com>
2023-09-28 13:03:27 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,14 +10,16 @@ int factorial(int n)
 	int fact;
 
 	if (n == 0 || n == 1)
-	{
+	
 		return (1);
-	}
+	
 
-	else
-	{
-		fact = n * factorial(n - 1);
-	}
+	if (n < 0)
+	        return (-1);
+
+        else
+	
+		fact = n * factorial(n - 1); 
 
 	return (fact);
 }
