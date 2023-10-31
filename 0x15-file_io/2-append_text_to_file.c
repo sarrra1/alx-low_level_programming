ceec415faf0391fa869781f8b33c868dac77c6a2
sarrra1 <sarafaisal0161999@gmail.com>
2023-10-30 17:07:01 -0700

    k

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ return (y);
 }
 
 /**
-*append_text_to_file- appends text to a file
+*append_text_to_file - appends text to a file
 *@filename: name of file to create
 *@text_content: text we want write
 *Return: 1 success 0 faill
@@ -29,7 +29,8 @@ return (y);
 int append_text_to_file(const char *filename, char *text_content)
 {
 int ayman;
-ssize_t dody = 0, lennn = stttrlen(text_content);
+ssize_t dody = 0;
+ssize_t lennn = stttrlen(text_content);
 /*int ayman; is like a fd */
 
 if (!filename)
@@ -44,5 +45,5 @@ if (lennn)
 dody = write(ayman, text_content, lennn);
 
 close(ayman);
-return (dody = lennn ? 1 : -1);
+return (dody == lennn ? 1 : -1);
 }
